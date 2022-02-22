ada1faf8392c0969fbc0431d28f343e93886ef3d
Damian Federico <damianfederico@protonmail.com>
2022-02-22 16:35:45 -0300

    Import httpClientTestingModule and RouterTestingModule + Add testcase to render 'Login Here' text

--- src/app/screens/login/login.component.spec.ts
+++ src/app/screens/login/login.component.spec.ts
@@ -1,16 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from "@angular/router/testing";
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let httpClient: HttpClient;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      imports: [ 
+        HttpClientTestingModule,
+        RouterTestingModule
+       ]
     })
     .compileComponents();
+    httpClient = TestBed.get(HttpClient);
   });
 
   beforeEach(() => {
@@ -21,5 +30,12 @@ describe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render "Login Here" text', () => {
+    const fixture = TestBed.createComponent(LoginComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h3')?.textContent).toContain('Login Here');
   });
 });

196f2379befc7d003400d6733849fb912d2a9dd8
Damian Federico <damianfederico@protonmail.com>
2022-02-24 11:54:34 -0300

    Import HttpClientTestingModule to pass home component, configuration and login services test

--- src/app/screens/home/home.component.spec.ts
+++ src/app/screens/home/home.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HomeComponent } from './home.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,7 +8,10 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent ],
+      imports: [ 
+        HttpClientTestingModule
+       ]
     })
     .compileComponents();
   });
